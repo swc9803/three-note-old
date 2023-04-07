5de11ebd5cd3df9dab3121470d77856dae4c2f15
swc9803 <swc9803@gmail.com>
2023-04-07 19:18:56 +0900

    초기 카메라 포지션, 타겟 설정, 애니메이션 ease 추가

--- src/pages/Test4.vue
+++ src/pages/Test4.vue
@@ -1,278 +1,14 @@
 <template>
 	<div class="wrapper">
-		<div ref="boxRef" class="box"></div>
+		<!-- <img src="@/assets/preview.webp" /> -->
+		<div class="pw">
+			<p>hi</p>
+			<p>hello</p>
+			<p>hello</p>
+			<p>hello</p>
+		</div>
 	</div>
 </template>
-
-<script setup>
-import gsap from 'gsap';
-
-const boxRef = ref();
-
-const sectionAni1 = gsap.timeline({ paused: true });
-const sectionAni2 = gsap.timeline({ paused: true });
-
-let startY = null;
-let currentY = null;
-let delta = null;
-let progress = 0;
-
-let isAnimated = false;
-
-let section1 = true;
-let section2 = false;
-let section3 = false;
-
-const onMouseDown = e => {
-	startY = e.clientY;
-	currentY = startY;
-};
-const onMouseMove = e => {
-	if (startY !== null) {
-		currentY = e.clientY;
-		delta = currentY - startY;
-		progress = Math.min(Math.abs(delta / window.innerHeight), 1);
-		if (section1) {
-			if (delta > 0) {
-				sectionAni1.pause();
-				sectionAni1.progress(progress);
-			}
-		} else if (section2) {
-			if (delta > 0) {
-				sectionAni2.pause();
-				sectionAni2.progress(progress);
-			} else if (delta <= 0) {
-				sectionAni1.pause();
-				sectionAni1.progress(1 - progress);
-			}
-		} else if (section3 && delta <= 0) {
-			sectionAni2.pause();
-			sectionAni2.progress(1 - progress);
-		}
-	}
-};
-const onMouseUp = () => {
-	if (section1) {
-		if (delta > 75) {
-			if (!isAnimated) {
-				sectionAni1.play();
-			}
-			section1 = false;
-			section2 = true;
-			console.log('move to 2');
-		} else {
-			sectionAni1.progress(0);
-		}
-	} else if (section2) {
-		if (delta > 75) {
-			if (!isAnimated) {
-				sectionAni2.play();
-			}
-			section2 = false;
-			section3 = true;
-			console.log('move to 3');
-		} else if (delta < -75) {
-			if (!isAnimated) {
-				sectionAni1.reverse();
-			}
-			section1 = true;
-			section2 = false;
-			console.log('move to 1');
-		} else {
-			if (delta < 0) {
-				if (!isAnimated) {
-					sectionAni1.play();
-				}
-			} else if (delta > 0) {
-				if (!isAnimated) {
-					sectionAni2.reverse();
-				}
-			}
-		}
-	} else if (section3) {
-		if (delta < -75) {
-			if (!isAnimated) {
-				sectionAni2.reverse();
-			}
-			section2 = true;
-			section3 = false;
-			console.log('move to 2');
-		} else {
-			sectionAni2.progress(1);
-		}
-	}
-	progress = 0;
-	startY = null;
-	currentY = null;
-	delta = null;
-};
-
-const onTouchStart = e => {
-	startY = e.touches[0].clientY;
-	currentY = startY;
-};
-const onTouchMove = e => {
-	if (startY !== null) {
-		currentY = e.touches[0].clientY;
-		delta = currentY - startY;
-		progress = Math.min(Math.abs(delta / window.innerHeight), 1);
-		if (section1) {
-			if (delta > 0) {
-				sectionAni1.pause();
-				sectionAni1.progress(progress);
-			}
-		} else if (section2) {
-			if (delta > 0) {
-				sectionAni2.pause();
-				sectionAni2.progress(progress);
-			} else if (delta <= 0) {
-				sectionAni1.pause();
-				sectionAni1.progress(1 - progress);
-			}
-		} else if (section3 && delta <= 0) {
-			sectionAni2.pause();
-			sectionAni2.progress(1 - progress);
-		}
-	}
-};
-const onTouchEnd = () => {
-	if (section1) {
-		if (delta > 75) {
-			if (!isAnimated) {
-				sectionAni1.play();
-			}
-			section1 = false;
-			section2 = true;
-			console.log('move to 2');
-		} else {
-			sectionAni1.progress(0);
-		}
-	} else if (section2) {
-		if (delta > 75) {
-			if (!isAnimated) {
-				sectionAni2.play();
-			}
-			section2 = false;
-			section3 = true;
-			console.log('move to 3');
-		} else if (delta < -75) {
-			if (!isAnimated) {
-				sectionAni1.reverse();
-			}
-			section1 = true;
-			section2 = false;
-			console.log('move to 1');
-		} else {
-			if (delta < 0) {
-				if (!isAnimated) {
-					sectionAni1.play();
-				}
-			} else if (delta > 0) {
-				if (!isAnimated) {
-					sectionAni2.reverse();
-				}
-			}
-		}
-	} else if (section3) {
-		if (delta < -75) {
-			if (!isAnimated) {
-				sectionAni2.reverse();
-			}
-			section2 = true;
-			section3 = false;
-			console.log('move to 2');
-		} else {
-			sectionAni2.progress(1);
-		}
-	}
-	progress = 0;
-	startY = null;
-	currentY = null;
-	delta = null;
-};
-
-const slideAnim = e => {
-	if (section1) {
-		if (e.deltaY > 0) {
-			if (!isAnimated) {
-				sectionAni1.play();
-			}
-			section1 = false;
-			section2 = true;
-			console.log('move to 2');
-		} else {
-			sectionAni1.progress(0);
-		}
-	} else if (section2) {
-		if (e.deltaY > 0) {
-			if (!isAnimated) {
-				sectionAni2.play();
-			}
-			section2 = false;
-			section3 = true;
-			console.log('move to 3');
-		} else if (e.deltaY < 0) {
-			if (!isAnimated) {
-				sectionAni1.reverse();
-			}
-			section1 = true;
-			section2 = false;
-			console.log('move to 1');
-		} else {
-			if (e.deltaY < 0) {
-				if (!isAnimated) {
-					sectionAni1.play();
-				}
-			} else if (e.deltaY > 0) {
-				if (!isAnimated) {
-					sectionAni2.reverse();
-				}
-			}
-		}
-	} else if (section3) {
-		if (e.deltaY < 0) {
-			if (!isAnimated) {
-				sectionAni2.reverse();
-			}
-			section2 = true;
-			section3 = false;
-			console.log('move to 2');
-		} else {
-			sectionAni2.progress(1);
-		}
-	}
-	progress = 0;
-	startY = null;
-	currentY = null;
-	delta = null;
-};
-
-onMounted(() => {
-	sectionAni1.to(boxRef.value, {
-		top: 200,
-		ease: 'none',
-		onStart: () => {
-			isAnimated = true;
-			console.log('s');
-		},
-	});
-	sectionAni2.to(boxRef.value, {
-		top: 400,
-		ease: 'none',
-		onStart: () => {
-			isAnimated = true;
-			console.log('s');
-		},
-	});
-	document.addEventListener('mousemove', onMouseMove);
-	document.addEventListener('mouseup', onMouseUp);
-	document.addEventListener('mousedown', onMouseDown);
-	document.addEventListener('wheel', slideAnim);
-	document.addEventListener('touchstart', onTouchStart);
-	document.addEventListener('touchmove', onTouchMove);
-	document.addEventListener('touchend', onTouchEnd);
-});
-</script>
 
 <style lang="scss" scoped>
 .wrapper {
@@ -280,20 +16,30 @@ onMounted(() => {
 	width: 100%;
 	height: 100vh;
 	overflow: hidden;
-	.box {
-		position: absolute;
-		top: 0;
-		width: 100px;
-		height: 100px;
-		background: red;
-		transition: top 0.3s ease-out;
-		z-index: 99;
-	}
-	p {
-		position: absolute;
-		top: 20%;
-		left: 50%;
-		z-index: 99;
+	background: #555555;
+	// img {
+	// 	position: absolute;
+	// 	height: 100%;
+	// 	object-fit: cover;
+	// }
+	.pw {
+		position: relative;
+		widows: 100%;
+		height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		// background: white;
+		z-index: 1;
+		p {
+			background-image: url('@/assets/preview.webp');
+			background-clip: text;
+			-webkit-background-clip: text;
+			color: transparent;
+			font-size: 6em;
+			font-weight: 700;
+		}
 	}
 }
 </style>
--- src/pages/Tunnel.vue
+++ src/pages/Tunnel.vue
@@ -7,7 +7,6 @@
 <script setup>
 import gsap from 'gsap';
 import * as THREE from 'three';
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const containerRef = ref();
 let camera;
@@ -19,21 +18,22 @@ scene.fog = new THREE.Fog(0x000000, 100, 200);
 // <polygon points="68.5,185.5 1,262.5 270.9,281.9 345.5,212.8 178,155.7 240.3,72.3 153.4,0.6 52.6,53.3 "/>
 const points = [
 	[191, 0],
-	[238.023, 135.279],
-	[381.211, 138.197],
-	[267.085, 224.721],
-	[308.557, 361.803],
+	[238, 135],
+	[381, 138],
+	[267, 224],
+	[308, 361],
 	[191, 280],
-	[73.4429, 361.803],
-	[114.915, 224.721],
-	[0.788696, 138.197],
-	[143.977, 135.279],
+	[73, 361],
+	[114, 224],
+	[0, 138],
+	[143, 135],
 	[191, 0],
 ];
 
+const yValues = [-125, 125];
 for (let i = 0; i < points.length; i++) {
 	const x = points[i][0];
-	const y = (Math.random() - 0.5) * 250;
+	const y = yValues[i % yValues.length];
 	const z = points[i][1];
 	points[i] = new THREE.Vector3(x, y, z);
 }
@@ -52,48 +52,55 @@ for (let i = 0; i < colors.length; i++) {
 	scene.add(tube);
 }
 
-const ani1 = gsap.timeline({ paused: true });
+const moveAni = gsap.timeline({ paused: true });
+
 let startProgress = 0;
 let endProgress = 1 / 3;
 let isAnimationPlaying = false;
 
-const process = () => {
-	if (isAnimationPlaying) return;
-	ani1.to(
-		{},
-		{
-			duration: 10 * (endProgress - startProgress),
-			onUpdate: () => {
-				const currentProgress =
-					ani1.progress() * (endProgress - startProgress) +
-					startProgress;
-				if (currentProgress < 0.995) {
-					const p1 = path.getPointAt(currentProgress);
-					gsap.to(camera.position, {
-						x: p1.x,
-						y: p1.y,
-						z: p1.z,
-						duration: 0.1,
-					});
-					const p2 = path.getPointAt(currentProgress + 0.001);
-					camera.lookAt(p2);
-				}
-			},
-			onComplete: () => {
-				isAnimationPlaying = false;
-				if (endProgress >= 1) {
-					startProgress = 0;
-					endProgress = 1 / 3;
-				} else {
-					startProgress = endProgress;
-					endProgress += 1 / 3;
-				}
-			},
-		},
-	);
+const customEasing = progress => {
+	if (progress < 0.5) {
+		return 2 * Math.pow(progress, 2);
+	} else {
+		return 1 - 2 * Math.pow(1 - progress, 2);
+	}
+};
 
-	isAnimationPlaying = true;
-	ani1.play();
+const process = () => {
+	if (!isAnimationPlaying) {
+		moveAni.clear();
+		moveAni.to(
+			{},
+			{
+				duration: 10 * (endProgress - startProgress),
+				onUpdate: () => {
+					const easedProgress = customEasing(moveAni.progress());
+					const currentProgress =
+						easedProgress * (endProgress - startProgress) +
+						startProgress;
+					if (currentProgress < 0.998) {
+						const p1 = path.getPointAt(currentProgress);
+						camera.position.set(p1.x, p1.y, p1.z);
+						const p2 = path.getPointAt(currentProgress + 0.001);
+						camera.lookAt(p2);
+					}
+				},
+				onComplete: () => {
+					isAnimationPlaying = false;
+					if (endProgress >= 1) {
+						startProgress = 0;
+						endProgress = 1 / 3;
+					} else {
+						startProgress = endProgress;
+						endProgress += 1 / 3;
+					}
+				},
+			},
+		);
+
+		isAnimationPlaying = true;
+		moveAni.play();
+	}
 };
 
 function animate() {
@@ -122,7 +129,11 @@ onMounted(() => {
 		0.1,
 		1000,
 	);
-	camera.position.set(238.023, 135.279, 100);
+
+	const p1 = path.getPointAt(0);
+	camera.position.set(p1.x, p1.y, p1.z);
+	const p2 = path.getPointAt(0.001);
+	camera.lookAt(p2);
 
 	init();
 	animate();
